37e8f646e12be2569d218411a68911af61966878
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2025-06-16 17:14:11 +0500

    Pushing Lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+    do{
+        if(head == NULL || fast_ptr->next == NULL){
+            return 0;
+        }
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr){
+            return 1;
+        }
+    }while(fast_ptr != NULL);
+    return 0;
 }
