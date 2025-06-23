e53117f91d4e10d223b5ccad717d28526c09c496
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2025-06-24 01:26:29 +0500

    Pushing Lab04

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -76,7 +76,11 @@ main:
 # a1 is the address of the "output" array (defined above).
 # Think: why might having a1 be useful?
 f:
-    # YOUR CODE GOES HERE!
+    la t0, output
+    addi t1, a0, 3
+    slli t1, t1, 2
+    add t1, t1, t0
+    lw a0, 0(t1)
 
     jr ra               # Always remember to jr ra after your function!
 
